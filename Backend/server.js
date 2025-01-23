4dba6bb71c47924e5f8bed9d5b817a4ea51a7796
shahbaz12243 <shahbazz12243@gmail.com>
2025-01-23 13:03:07 +0530

    server completed

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+const app = express();
+
+
+const PORT = 9090;
+
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
